4ec82b7c50b50534d4e100fc154eb159650e21cd
Clement Guyon <clement.guyon@ynov.com>
2023-06-04 12:17:18 +0200

    fix: architecture

--- test/test.dart
+++ test/test.dart
@@ -1,5 +1,5 @@
 import 'repository/repository_tests.dart' as repository_test;
-import 'tools_tests.dart' as tools_tests;
+import 'tool/tools_tests.dart' as tools_tests;
 
 void main() {
   tools_tests.main();
